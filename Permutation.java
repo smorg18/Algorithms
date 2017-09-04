6bb11b382aa383630e9ebe5ac13423841bd38b56
Savannah Morgan <savannahmorgan@Savannahs-MacBook-Pro.local>
2017-09-04 14:27:18 -0400

    the other two algorithms base file

---
+++ Permutation.java
@@ -0,0 +1,64 @@
+
+//import
+import java.io.*;
+
+import java.util.HashMap; 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.HashMap; 
+public class Permutation {
+    //get weight and value sum of set 
+    public static int greatestIndexI (ArrayList<Integer>perm, int n){
+        int greatestIndex =0;
+        for (int i = 0; i<(n-1); i++){
+            if (perm[i]<perm[i+1]){
+                greatestIndex = i;
+            }
+        }
+        return greatestIndex;
+    }
+    public static int greatestIndexJ (ArrayList<Integer>perm, int n, i){
+        int greatestIndex =0;
+        for (int j = 0; j<n; j++){
+            if (perm[i]<perm[j]){
+                greatestIndex = j;
+            }
+        }
+        return greatestIndex;
+    }
+
+    public static int BestSet(ArrayList<Integer>binaryList, HashMap<String, Integer[]> dict ){
+
+
+    }
+    public static void main(String [] args) {
+            // get info from system
+            Scanner sc = new Scanner(System.in);
+            int n = sc.nextint();
+            ArrayList<Integer> perm = new ArrayList<>();
+            for (int l=0; l< n; l++)
+            {
+                perm.add(l+1);
+            }
+            
+           
+           //System.out.println(dict.get("TV")[);
+            // int result = dict.get(keys[0])[1];
+            // test.add(dict.get(keys.get(0))[1]);
+            // test.add(1);
+            // System.out.println(weight(test)); 
+                
+        }
+        catch(FileNotFoundException ex) {
+            System.out.println(
+                "Unable to open file '" + fileName + "'");                
+        }
+        catch(IOException ex) {
+            System.out.println(
+                "Error reading file '" + fileName + "'");                  
+        
+        
+        }
+        
+    }
+}
---
+++ TSP.java
@@ -0,0 +1,94 @@
+//import
+import java.io.*;
+
+import java.util.HashMap; 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.HashMap; 
+public class TSP {
+    //get weight and value sum of set 
+    public static int getSum (ArrayList<Integer>setWeights){
+        int sum =0;
+        for (int i = 0; i<setWeights.size(); i++){
+            sum = sum+setWeights.get(i);
+        }
+        return sum;
+    }
+    public static int BestSet(ArrayList<Integer>binaryList, HashMap<String, Integer[]> dict ){
+
+
+    }
+    public static void main(String [] args) {
+        // file
+        String fileName = "knap_input.txt";
+
+        // This will reference one line at a time
+        String line = null;
+
+        try {
+            //read file 
+            FileReader fileReader = 
+                new FileReader(fileName);
+
+            //  BufferedReader to read lines 
+            BufferedReader bufferedReader = 
+                new BufferedReader(fileReader);
+            //keep a list of keys and values for dictionary of objects
+            List<String> keys = new ArrayList<>();
+            List<Integer> values = new ArrayList<>();
+            //index to determine type of info retrieved (name, weight or value?)
+            int i =0;
+            while((line = bufferedReader.readLine()) != null) {
+                
+                //index =0 then it is the name/key
+                if (i == 0){ 
+                    keys.add(line);
+                    i=1;   
+                }   
+                //other two options are added to values list 
+                else if (i==1){
+                    int result = Integer.parseInt(line);
+                    values.add(result);
+                    i = 2;
+                }
+                else{
+                    int result = Integer.parseInt(line);
+                    values.add(result);
+                    i=0;
+                }
+            }
+            // close file
+            bufferedReader.close();   
+            HashMap<String, Integer[]> dict = new HashMap<String, Integer[]>();
+            int k=0;
+            int n= keys.size();
+            //System.out.println(values.size());
+            for (int l=0; l< n; l++)
+            {
+                Integer[] info = {values.get(k), values.get(k+1),i};
+                //System.out.println(keys.get(l));
+                dict.put(keys.get(l), info);
+                k=k+2;
+            }
+            ArrayList<Integer> test = new ArrayList<>();
+           
+           //System.out.println(dict.get("TV")[);
+            // int result = dict.get(keys[0])[1];
+            // test.add(dict.get(keys.get(0))[1]);
+            // test.add(1);
+            // System.out.println(weight(test)); 
+                
+        }
+        catch(FileNotFoundException ex) {
+            System.out.println(
+                "Unable to open file '" + fileName + "'");                
+        }
+        catch(IOException ex) {
+            System.out.println(
+                "Error reading file '" + fileName + "'");                  
+        
+        
+        }
+        
+    }
+}
