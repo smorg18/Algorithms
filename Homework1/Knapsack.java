5c872c274b7495ee434f3ef9405ab81bd51221a7
Savannah Morgan <savannahmorgan@Savannahs-MacBook-Pro.local>
2017-09-04 23:06:47 -0400

    i

--- Homework1/Knapsack.java
+++ Homework1/Knapsack.java
@@ -1,11 +1,16 @@
-//import
+//-----------------import------------------------
 import java.io.*;
 import java.util.LinkedList;
 import java.util.HashMap; 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.HashMap; 
+
+
+//-----------------start of class-----------------
 public class Knapsack {
+
+//-------------lexicographic binary---------------
     public static void binaryPerm(String set, int recursion, List<String> results) {
 
         if(recursion == 0) {
@@ -16,29 +21,37 @@ public class Knapsack {
             binaryPerm(set+ "1", recursion - 1, results);
         }
     }
-    //get weight and value sum of set 
-    public static int getSum (List<Integer>setWeights){
+//-------get weight and value sum of set---------
+    public static int getSum (List<Integer>setAmounts){
         int sum =0;
-        for (int i = 0; i<setWeights.size(); i++){
-            sum = sum+setWeights.get(i);
+        for (int i = 0; i<setAmounts.size(); i++){
+            sum = sum+setAmounts.get(i);
         }
         return sum;
     }
+//----------find best bang for buck--------------
     public static String BestSet(List<String>binaryList, HashMap<String, Integer[]> dict, int keySize, List<String> keys, int limit ){
-        List<Integer> setWeight = new ArrayList<>();
-        List<Integer> setValues = new ArrayList<>();
+        
         int greatestValue = 0;
         String greatestSet = "";
+//----------go through each possible set and check values--------------
         for (int i =0; i < binaryList.size(); i++){
+            List<Integer> setWeight = new ArrayList<>();
+            List<Integer> setValues = new ArrayList<>();
             String binarySet = binaryList.get(i);
+//----------check which item is picked from each possible set--------------
             for(int k = 0; k< keySize; k++){
                 int itemIndex = dict.get(keys.get(k))[2];
+                //System.out.println(itemIndex);
+//--------------if in set calcualte weights and values --------------------
                 if (binarySet.charAt(itemIndex) == '1'){
                     setValues.add(dict.get(keys.get(k))[1]);
                     setWeight.add(dict.get(keys.get(k))[0]);
                 }
             }
+//--------------if not too heavy check if best value  --------------------
             if (getSum (setWeight) <= limit){
+                System.out.println(getSum(setValues));
                 int value = getSum(setValues);
                 if (value > greatestValue){
                     greatestValue= value;
@@ -50,24 +63,22 @@ public class Knapsack {
                 
             
         }
-        System.out.println(greatestSet);
+        //System.out.println(greatestValue);
         return greatestSet;
 
     }
+//----------------- Main --------------------
     public static void main(String [] args) {
-        // file
-        List<String> results = new LinkedList<String>();
-        binaryPerm("", 4, results);
-        //System.out.println(results.size());
         
+        //open input fike
         String fileName = "knap_input.txt";
 
         // This will reference one line at a time
         String line = null;
         System.out.println("Command-line arguments:");
-        int n = 0;
+        int limit = 0;
         for (String arg : args) {
-                n = Integer.parseInt(args[0]);
+                limit = Integer.parseInt(args[0]);
                 System.out.println(n);
         }
         try {
@@ -103,21 +114,29 @@ public class Knapsack {
                 }
             }
             // close file
-            bufferedReader.close();   
+            bufferedReader.close(); 
+            
             HashMap<String, Integer[]> dict = new HashMap<String, Integer[]>();
             int k=0;
             int numKeys= keys.size();
+//----------------Create binary perm-----------------------------
+            List<String> results = new LinkedList<String>();
+            binaryPerm("", numKeys, results);  
             //System.out.println(values.size());
+//----------------Create dictioanry with item name as key ------------------------
+//----------------------- weight and price as values -----------------------------
+//--------------------- -----------------------------
+
             for (int l=0; l< numKeys; l++)
             {
-                Integer[] info = {values.get(k), values.get(k+1),i};
+                Integer[] info = {values.get(k), values.get(k+1),l};
                 //System.out.println(keys.get(l));
                 dict.put(keys.get(l), info);
                 k=k+2;
             }
             ArrayList<Integer> test = new ArrayList<>();
            
-        String set = BestSet(results, dict, numKeys, keys, 20 );
+        String set = BestSet(results, dict, numKeys, keys, 43);
         System.out.println(set);  
                 
         }
