2e92681734f6b944b6bceff80b9ca707b653f0e9
Savannah Morgan <savannahmorgan@Savannahs-MacBook-Pro.local>
2017-09-05 01:00:45 -0400

    changes to tsp

--- Homework1/TSP.java
+++ Homework1/TSP.java
@@ -6,16 +6,21 @@ import java.util.HashMap;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.HashMap; 
-public class TSP{
+public class TSP {
     //get weight and value sum of set 
    
-    public static int calcualteCost(ArrayList<Integer>perm, HashMap<String, ArrayList<Integer>> dict, int numCities){
+    public static int calcualteCost(ArrayList<Integer>perm, HashMap<Integer, List<Integer>> dict, int numCities){
         int totalCost=0;
-        for (int index=0; index<numCities; index++){
+        for (int index=0; index<numCities-1; index++){
             int currentCity = perm.get(index);
             int destinationCity = perm.get(index+1);
             int cost = dict.get(currentCity).get(destinationCity - 1);
             totalCost= totalCost+ cost;
+            if (index == numCities-2){
+                destinationCity = perm.get(0);
+                cost = dict.get(currentCity).get(destinationCity - 1);
+                totalCost= totalCost+ cost;
+            }
         }   
         return totalCost;    
     }
@@ -50,12 +55,12 @@ public class TSP{
     
     public static void main(String [] args) {
         
-        System.out.println("Command-line arguments:");
+        //System.out.println("Command-line arguments:");
         int n = 0;
-        for (String arg : args) {
-                n = Integer.parseInt(args[0]);
-                System.out.println(n);
-        }
+        // for (String arg : args) {
+        //         //n = Integer.parseInt(args[0]);
+        //         System.out.println(n);
+        // }
         String fileName = "tsp_input.txt";
          try 
         {
@@ -71,18 +76,19 @@ public class TSP{
             
             //index to determine type of info retrieved (name, weight or value?)
             int city =1;
-            HashMap<String, ArrayList<Integer>> dict = new HashMap<String, ArrayList<Integer>>();
+            HashMap<Integer, List<Integer>> dict = new HashMap<Integer, List<Integer>>();
             String firstLine= bufferedReader.readLine();
             String[] splitLine = firstLine.split("\\s+");
-            int numOfCities = Integer.parseInt(splitLine[1]);
-
+            n = Integer.parseInt(splitLine[1]);
+            //System.out.println(numOfCities);
             while((line = bufferedReader.readLine()) != null) {
+                
                 String[] splited = line.split("\\s+"); 
                 //index =0 then it is the name/key
                 
                 //other two options are added to values list
                 List<Integer> values = new ArrayList<>(); 
-                for (int l =0; l<numOfCities; l++){
+                for (int l =0; l<n; l++){
                     values.add(Integer.parseInt(splited[l])); 
                 }
                 dict.put(city,values);
@@ -95,23 +101,33 @@ public class TSP{
                 
         
             try{
-                BufferedWriter writer = new BufferedWriter(new FileWriter("sample_perm_output.txt"));
+                BufferedWriter writer = new BufferedWriter(new FileWriter("tsp_output.txt"));
                 ArrayList<Integer> perm = new ArrayList<>();
                 for (int l=0; l< n; l++)
                 {
                     perm.add(l+1);
                 }
-                String Line = Integer.toString(perm.get(0));
-                for (int c = 1; c < n; c++){
-                    Line = Line + " " + Integer.toString(perm.get(c));
-                }
-                try{
-                        Line= Line + "\n";
+                
+                String Line = Integer.toString(perm.get(0)-1);
+                int bestCost = calcualteCost(perm,dict, n);
+                String bestPerm = Integer.toString(perm.get(0));
+                    //System.out.println(cost);
+                    
+                    //System.out.println(perm.get(0));
+                    for (int c = 1; c < n; c++){
+                        Line = Line + "->" + Integer.toString(perm.get(c)-1);
+                        
+                    }
+                    try{
+                        Line = Line + "->" + Integer.toString(perm.get(0)-1);
+                        Line= Line + "   Score: "+ Integer.toString(bestCost)+"\n";
                         writer.write(Line);
                     }
-                catch  (IOException e) {
-                    System.out.println("Failed to write to file ");
-                }
+                    catch  (IOException e) {
+                        System.out.println("Failed to write to file ");
+                    }
+                
+                
                  while (orderCheck(perm,n) == 1){
                     int i = greatestIndexI(perm, n);
                     int j = greatestIndexJ(perm,n,i);
@@ -127,19 +143,39 @@ public class TSP{
                     perm.set((n-1),oldM);
 
                 //}
-                    int cost = calcualteCost(perm,dict, numOfCities);
-                    String newLine = Integer.toString(perm.get(0));
+                    int cost = calcualteCost(perm,dict, n);
+                    
+                        
+                    
+                    //System.out.println(cost);
+                    String newLine = Integer.toString(perm.get(0)-1);
+                    //System.out.println(perm.get(0));
                     for (int c = 1; c < n; c++){
-                        newLine = newLine + "->" + Integer.toString(perm.get(c));
+                        newLine = newLine + "->" + Integer.toString(perm.get(c)-1);
+                        
+                    }
+                    newLine = newLine + "->" + Integer.toString(perm.get(0)-1);
+                    if (cost < bestCost){
+                        bestCost= cost;
+                        bestPerm = newLine;
                     }
                     try{
-                        newLine= newLine + "Score: "+ Integer.toString(cost)+"\n";
+                        newLine= newLine + "   Score: "+ Integer.toString(cost)+"\n";
                         writer.write(newLine);
                     }
                     catch  (IOException e) {
                         System.out.println("Failed to write to file ");
                     }
                     
+                }
+                try{
+                        String costOutput=  "The cheapest tour costs: "+ Integer.toString(bestCost)+"\n";
+                        writer.write(costOutput);
+                        String tourOutput=  "The cheapest tour was: "+ bestPerm+"\n";
+                        writer.write(tourOutput);
+                }
+                catch  (IOException e) {
+                    System.out.println("Failed to write to file ");
                 }
                 try {
                         writer.close();
