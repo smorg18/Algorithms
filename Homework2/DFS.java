175fb67cc0ea42f1d114d0789fa2ec615a585934
Savannah Morgan <savannahmorgan@Savannahs-MacBook-Pro.local>
2017-09-25 17:28:41 -0400

    testing

--- Homework2/DFS.java
+++ Homework2/DFS.java
@@ -24,21 +24,23 @@ public class DFS {
 
 
 //------------------ DFS --------------------
-    public static int DFS (Integer v, Stack vertexStack, Integer marked[][] ,  HashMap<Integer, List<Integer>> verts ){
+    public static int DFS (Integer v, Stack vertexStack, Integer marked[] ,  HashMap<Integer, List<Integer>> verts ){
         //verticie marked as visited and with count of order visited
         // also kept in stack for  cyclic checking 
-        marked [v][0] = 1;
+        //marked [v][0] = 1;
+        marked[v]= 1;
         count ++; 
-        marked [v][1] = count; 
+        //marked [v][1] = count; 
         vertexStack.push(v);
         List<Integer> neighbors = verts.get(v);
-
+        System.out.println(neighbors.size());
         //recurion for all adjacent neighbors 
         //if adjacent neighbor already visited and in the stack than cyclic
         for (int adjI= 0; adjI< neighbors.size(); adjI++){
             Integer adjV = neighbors.get(adjI); 
-            if (marked[adjV][0] == 0){
+            if (marked[adjV] == 0){
                 if (DFS(adjV, vertexStack, marked, verts)== 1){
+                     System.out.println("here"+adjV);
                     return 1;
                 }
             }
@@ -48,6 +50,7 @@ public class DFS {
             } 
         }
         vertexStack.pop();
+        vertexStack.remove(v);
         return 0;
             
             
@@ -62,9 +65,11 @@ public class DFS {
 
 
 //------------ IS CYCLIC OR NAH -------------
-    public static int CyclicChecker (int numOfVerts, Stack vertexStack, Integer marked[][] ,  HashMap<Integer, List<Integer>> verts){
+    public static int CyclicChecker (int numOfVerts, Stack vertexStack, Integer marked[] ,  HashMap<Integer, List<Integer>> verts){
+        
         for (int v= 0; v< numOfVerts; v++){
-            if (marked[v][0]==0){
+            if (marked[v]==0){
+                
                 if (DFS(v,vertexStack, marked,verts) == 1){
                     return 1;
                 }
@@ -101,23 +106,24 @@ public class DFS {
             HashMap<Integer,  List<Integer>> verts = new HashMap<Integer, List<Integer>>();
             int len =0;
             int v = -1;
-            Integer[][] marked = new Integer[500][];
+            Integer[] marked = new Integer[500];
             while((line = bufferedReader.readLine()) != null) {
                 v++;
                 String[] newLine = line.split("\\s+"); 
                 len = newLine.length;
+                //System.out.println(len);
                 List<Integer> neighbors = new ArrayList<>(); 
-                int k = -1;
+                //int k = -1;
                 for (int i = 0; i<len; i++){
                     if (Integer.parseInt(newLine[i]) == 1 ){
                         //add to array neighbors
-                        k++;
-                        neighbors.set(k, i);
+                        //k++;
+                        neighbors.add(i);
                     }
                 }
                 //initialize amrk 
-                marked[k][0] = 0;
-                marked[k][1] = -1; 
+                marked[v] = 0;
+                //marked[v] = -1; 
                 // add v(vertex) as key and add neighbor as value  
                 verts.put( v, neighbors);
             }
@@ -135,7 +141,7 @@ public class DFS {
             }
             //end program 
 //(Integer v, Stack vertexStack, Integer marked[][] ,  HashMap<Integer, List<Integer>> verts ){
-            else if (cycleResult==0){
+            if (cycleResult==0){
                 System.out.println("NO CYCLE");
             }
             // close file
