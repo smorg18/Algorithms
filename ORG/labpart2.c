1b4a40e21d9f6f17901b436eea682ce51a5f6fe6
Savannah Morgan <savannahmorgan@Savannahs-MacBook-Pro.local>
2017-10-20 09:21:00 -0400

    part 2 of assembly language

---
+++ ORG/labpart2.c
@@ -0,0 +1,53 @@
+
+
+int fcn2(int v5, int  v6){
+    return v6 +v5;
+}
+
+int fcn1(v1, v2 ,v3, v4, v5, v6){
+    /* HANDLE
+        ALL 
+        PUSH 
+        FUCNTIONS
+    */
+
+    int value5 = v5;
+    blah = v4;
+    blah = v3;
+    blah = v2;
+    blah = v1;
+    fcn2();
+    blah = vlah;
+    v5 = blah;
+    v6 = blah;
+
+    fcn2();
+
+    blah = blah +blah;
+    blah = blah + blah 
+
+    /* 
+    HANDLE 
+    ALL 
+    THE 
+    POPS
+    */
+}
+
+
+
+
+
+
+
+
+int main(){
+    int v1 = 5;
+    int v2 = 1;
+    int v3 = 3;
+    int v4 = 10;
+    int v5 = 7;
+    int v6 = 2; 
+    return fcn1 (v1, v2, v3, v4 , v4, v5, v6);
+
+}
